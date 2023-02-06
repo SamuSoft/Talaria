a4909289603a571b711ca31a74f9a6322682d760
Samuel <samuelph@kth.se>
2023-02-06 12:57:08 +0100

    Adds final features to portal

--- portal/src/main/java/com/talaria/portal/clients/MessageService.java
+++ portal/src/main/java/com/talaria/portal/clients/MessageService.java
@@ -2,9 +2,14 @@ package com.talaria.portal.clients;
 
 import com.talaria.portal.entities.Message;
 import com.talaria.portal.repositories.MessageRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.UUID;
 
 @Service("messageService")
@@ -13,8 +18,43 @@ public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
 
-    public Message getMessageById(UUID id){
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Message getMessageById(UUID id) {
         return messageRepository.getReferenceById(id);
+    }
+
+    public List<Message> getMessagesByDate(String receiver, Timestamp from, Timestamp until) {
+        var query = entityManager.createQuery(
+                "SELECT m FROM Message m WHERE m.receiverMedium = :receiver AND " +
+                        "(m.sent >= :from OR m.sent <= :until)", Message.class);
+
+        return query.setParameter("receiver", receiver)
+                .setParameter("from", from)
+                .setParameter("until", until)
+                .getResultList();
+    }
+
+    public List<Message> getUnreadMessages(String receiver) {
+        // This whole thing should really just be one single DB Transaction
+        var query = entityManager.createQuery(
+                "SELECT m FROM Message m WHERE m.receiverMedium = :receiver AND m.received = false",
+                Message.class);
+
+        var Messages = query.setParameter("receiver", receiver)
+                .getResultList();
+
+        for (var m : Messages) {
+            // Looping over all messages, but as said above, should be something else
+            entityManager.createQuery("UPDATE Message m SET m.received = true WHERE m.id = :id")
+                    .setParameter("id", m.uuid);
+        }
+        return Messages;
+    }
+
+    public void deleteMessage(UUID id){
+        messageRepository.deleteById(id);
     }
 
     public void addMessage(Message message) {
--- portal/src/main/java/com/talaria/portal/endpoints/MessageEndpoints.java
+++ portal/src/main/java/com/talaria/portal/endpoints/MessageEndpoints.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -19,18 +21,43 @@ public class MessageEndpoints {
 
     List<Message> messages = new ArrayList<>();
 
+    private Timestamp parseDate(String date) {
+        var nDate = date;
+        if (date.contains("+"))
+            nDate = date.substring(0, date.indexOf('+'));
+        return Timestamp.valueOf(nDate.replace('T', ' '));
+    }
+
     @Resource(name = "queueService")
     private QueueService queueService = new QueueService();
     @Resource(name = "messageService")
     private MessageService messageService = new MessageService();
 
     @GetMapping("/messages")
-    public List<Message> getAllMessagesSentBetween(Timestamp from, Timestamp until) {
-        return messages.stream()
-                .filter(message ->
-                        (from != null && message.sent.after(from)) ^
-                                (until != null && message.sent.before(until)))
-                .toList();
+    public List<Message> getAllMessagesSentBetween(String user, String from, String until) {
+        //Doing a lot of string magic because Timestamp cannot be parsed again from its string format
+        Timestamp fromT = null;
+        Timestamp untilT = null;
+
+        // If from time isn't set, only retrieve 1 day back
+        if (from == null)
+            fromT = Timestamp.from(Instant.now().minus(1, ChronoUnit.DAYS));
+        else
+            fromT = parseDate(from);
+
+        // If until time isn't set, set to now
+        if (until == null)
+            untilT = Timestamp.from(Instant.now());
+        else
+            untilT = parseDate(until);
+
+
+        return messageService.getMessagesByDate(user, fromT, untilT);
+    }
+
+    @GetMapping("/messages/unread")
+    public List<Message> getUnreadMessages(String user) {
+        return messageService.getUnreadMessages(user);
     }
 
     @GetMapping("/messages/{uuid}")
@@ -43,14 +70,8 @@ public class MessageEndpoints {
 
     @DeleteMapping("/messages/{uuid}")
     public ResponseEntity<Message> deleteMessage(@PathVariable UUID uuid) {
-        var maybeMessage = messages.stream()
-                .filter(message -> message.uuid.equals(uuid))
-                .findFirst();
-        if (maybeMessage.isPresent()) {
-            messages.remove(maybeMessage.get());
-            return new ResponseEntity<>(HttpStatus.ACCEPTED);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        messageService.deleteMessage(uuid);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
 
